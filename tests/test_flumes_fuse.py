71af0d92c8c82731c629199f652aac9a0e578c64
Michalis Dimopoulos <85113599+mdimopoulos@users.noreply.github.com>
2022-10-28 16:13:20 +0200

    COM-4474: Fix for test_path test (#4)
    test_flumes_fuse.py
    + Adapted test_path to match the latest changes done in flumes-fuse design
    + Added relevant comments

--- tests/test_flumes_fuse.py
+++ tests/test_flumes_fuse.py
@@ -5,24 +5,30 @@ from flumes.options import Options
 from flumes.schema import Audio, File, Info, Schema, Video
 
 from flumes_fuse import __version__
-from flumes_fuse.path import PathParser, RootPath
+from flumes_fuse.fs import Root
 
 
 def test_version():
+    """The purpose of this test is to verify that the version of the package
+    is the expected"""
     assert __version__ == "0.1.2"
 
 
-def test_path():
-    options = Options()
+def test_root_path():
+    """The purpose of this test is to verify that the content of root path
+    used by fuse to generate the filesystem is as expected"""
+
     # Create a schema in SQLAlchemy with sqlite in memory
-    args = options.parse_args(["-i", "sqlite://"])
+    options = Options()
+    args = options.parse_args(["-i", "sqlite://", "-b", ":memory:"])
     config = Config(args)
     schema = Schema(config)
-    # Add some data
+
+    # Create an SQL session and add a dummy file entry to the database
     session = schema.create_session()
-    f = File(name="test.mp4", path="", mtime=datetime.now())
-    info = Info(
-        file=f,
+    dummy_file = File(name="test.mp4", path="", mtime=datetime.now())
+    dummy_info = Info(
+        file=dummy_file,
         video_streams=1,
         audio_streams=1,
         subtitle_streams=0,
@@ -30,15 +36,27 @@ def test_path():
         duration=5000000000,
         live=False,
     )
-    audio = Audio(
-        info=info, media_type="audio/mpeg", bitrate=131072, channels=2, depth=8
+    dummy_audio = Audio(
+        info=dummy_info, media_type="audio/mpeg", bitrate=131072, channels=2, depth=8
     )
-    video = Video(info=info, media_type="video/x-h264", width=1920, height=1080)
+    dummy_video = Video(
+        info=dummy_info, media_type="video/x-h264", width=1920, height=1080
+    )
 
-    session.add(video)
-    session.add(audio)
+    session.add(dummy_video)
+    session.add(dummy_audio)
     session.commit()
-    parser = PathParser(schema)
-    path = parser.parse("/")
-    assert type(path) == RootPath
-    # parser.parse("/files/{}".format(f.id))
+
+    # Initialize root path based on current SQL session
+    root = Root(session)
+
+    # Expected values for root path content
+    expected_root_path_content = [".", ".."]
+    for i in range(len(root.cls_paths)):
+        expected_root_path_content.append(str(root.cls_paths[i][0]))
+
+    # Actual values of root path content
+    actual_root_path_content = [entry.name for entry in root.readdir(0)]
+
+    # Assert that the actual content of root path coincides with the expected
+    assert actual_root_path_content == expected_root_path_content
